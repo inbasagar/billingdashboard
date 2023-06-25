39df8cbe93bab28998f15ec28fc8cd802a3bcb23
inbasagar <inba172001@gmail.com>
2023-06-25 18:19:51 +0530

    Update axios.js

--- src/Redux/axios.js
+++ src/Redux/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const Axios = axios.create({
-    baseURL: 'https://ecommerceserver-wyky.onrender.com/',
+    baseURL: process.env.REACT_APP_API_URL,
   });
   
   export default Axios;
